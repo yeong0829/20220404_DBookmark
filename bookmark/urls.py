3d07ff547ad31c2e6b00a0aa9afe293e6ba54dc1
yeong0829 <w2017@e-mirim.hs.kr>
2022-04-22 13:51:20 +0900

    templates bookmark_update, bookmark_list

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetilView
+from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetilView, BookmarkUpdateView
 
 app_name = 'bookmark'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('list/', BookmarkListView.as_view(), name='list'),  #bookmark:list
     path('add/', BookmarkCreateView.as_view(), name='add'),  #bookmark:add
     path('detail/<int:pk>', BookmarkDetilView.as_view(), name='detail'),   #bookmark:detail
+    path('edit/<int:pk>', BookmarkUpdateView.as_view(), name='edit'),   #bookmark:edit
 ]
