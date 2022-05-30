f121d402a52ef63a5aa5ce7b0bc20970d4b1d455
yeong0829 <w2017@e-mirim.hs.kr>
2022-05-30 13:56:29 +0900

    heroku 설정

--- DBookmark/settings.py
+++ DBookmark/settings.py
@@ -139,3 +139,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # MEDIA(사용자가 업로드한 파일)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')  #업로드한 파일 위치
+
+
+#git push keroku feature/heroku:master 에서 오류남
+#feature/heroku
+#heroku run python .\manage.py migrate
